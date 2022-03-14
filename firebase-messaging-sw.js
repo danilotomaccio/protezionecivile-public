505ef3d4ce1835fb983e0ce45311a9062ff2ed6a
Danilo Tomaccio <danilo.tomaccio@gmail.com>
2022-03-14 10:18:54 +0000

    0.1.4

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-const firebaseConfig = {
+/* const firebaseConfig = {
     apiKey: "AIzaSyCGLXls0uwohW8UczwMuMPgY61QRCyjydA",
     authDomain: "soter-ia.firebaseapp.com",
     databaseURL: "https://soter-ia.firebaseio.com",
@@ -10,8 +10,8 @@ const firebaseConfig = {
     messagingSenderId: "712755300293",
     appId: "1:712755300293:web:9c91cc4c98e2a36b77edac",
     measurementId: "G-6Y82RJ5Q37",
-};
-/* 
+}; */
+
 const firebaseConfig = {
     apiKey: "AIzaSyC_5sSUzZkMofwuI4f6uF1CdKa5uBBzSU4",
     authDomain: "pcgltricarico05.firebaseapp.com",
@@ -21,7 +21,7 @@ const firebaseConfig = {
     messagingSenderId: "320152077505",
     appId: "1:320152077505:web:f4c84686f061134a78c040",
     measurementId: "G-7ZHL4J15XG"
-}; */
+};
 
 const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
